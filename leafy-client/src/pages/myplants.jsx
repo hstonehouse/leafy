098ba55404ebc65763a921191ec0967b19355d51
Helen <hln.stonehouse@gmail.com>
2021-12-12 13:50:59 +1100

    implemented search bar toggling functionality

--- leafy-client/src/pages/myplants.jsx
+++ leafy-client/src/pages/myplants.jsx
@@ -5,6 +5,7 @@ import { GridImage } from '../components/grid-image';
 
 export function MyPlants() {
     const [sidebar, setSidebar] = useState(false);
+    const [searchBar, setSearchBar] = useState(false);
     
     const showSideBar = () => {
         const sidebarDiv = document.querySelector(".sidebar");
@@ -14,10 +15,22 @@ export function MyPlants() {
             sidebarDiv.style.display = "none";
             setSidebar(false);
         } else {
-            console.log("blah");
             sidebarDiv.style.display = " ";
             sidebarDiv.style.display = "block";
             setSidebar(true);
+        }
+    }
+
+    const showSearchBar = () => {
+        const searchbarDiv = document.querySelector("#searchbar");
+        if (searchBar) {
+            searchbarDiv.style.display = " ";
+            searchbarDiv.style.display = "none";
+            setSearchBar(false);
+        } else {
+            searchbarDiv.style.display = " ";
+            searchbarDiv.style.display = "flex";
+            setSearchBar(true);
         }
     }
 
@@ -34,9 +47,22 @@ export function MyPlants() {
             <nav className="is-flex is-justify-content-space-around" id="navbar">
                 <FontAwesomeIcon icon="bars" size="2x" id="burger" onClick={showSideBar}/>
                 <p>My Plants</p>
-                <FontAwesomeIcon icon="plus-square" size="2x" id="add"/>
+                <FontAwesomeIcon icon="plus-square" size="2x" id="add" onClick={showSearchBar}/>
             </nav>
-          
+
+            <div className="level-item" id="searchbar">
+                <div className="field has-addons">
+                    <p className="control">
+                        <input className="input" type="text" placeholder="Search Plants"></input>
+                    </p>
+                    <p className="control">
+                        <button className="button">
+                            Search
+                        </button>
+                    </p>
+                </div>
+            </div>
+            
             <p>Click + to add your first plant.</p>
 
             {/* <div className="all-plants">
